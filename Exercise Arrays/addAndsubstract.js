0587a84dae7cc67e9c2d0cdae8ea1840e2b34319
MaxSpasov <picachu@abv.bg>
2022-10-08 12:06:19 +0300

    Submitted solutions to Exercise Arrays

---
+++ Exercise Arrays/addAndsubstract.js
@@ -0,0 +1,26 @@
+function addAndSubstract(input){
+
+    let resultArray = [ ];
+    let inputArraySum = 0;
+    let resultArraySum = 0;
+    let inputLength = input.length;
+    for (let i = 0; i < inputLength; i++) {
+
+        let currentNumber = input[i];
+
+        let odd = (currentNumber - i);
+        let even = (currentNumber + i);
+
+        if(currentNumber % 2 === 0) {
+            resultArray.push(even);
+        } else {
+            resultArray.push(odd);
+        }
+        inputArraySum += currentNumber;
+        resultArraySum += resultArray[i];
+    }
+    console.log(resultArray);
+    console.log(inputArraySum);
+    console.log(resultArraySum);
+}
+addAndSubstract([-5, 11, 3, 0, 2])
---
+++ Exercise Arrays/maxNumber.js
@@ -0,0 +1,18 @@
+function maxNumber(arr) {
+    let resultArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let isBigger = true;
+        let currentNumber = arr[i];
+        for (let k = i + 1; k < arr.length; k++) {
+            if (currentNumber <= arr[k]) {
+                isBigger = false;
+            }
+        }
+        if (isBigger) {
+            resultArray.push(arr[i]);
+        }
+    }
+console.log(resultArray.join(' '));
+}
+maxNumber([1, 4, 3, 2])
